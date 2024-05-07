f57676916dc791d8c680fbbd6579e3c9f85a5706
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-05-07 20:07:04 +0200

    post?

--- public/script/main.js
+++ public/script/main.js
@@ -1,19 +1,47 @@
 var checkboxes = document.querySelectorAll('input[type="checkbox"]')
+var checkboxOne = document.querySelector('input[type="checkbox"]:nth-of-type(1)')
 var submit = document.querySelector('button')
+var selecteerDrie = document.querySelector('.selecteer_3')
 
 checkboxes.forEach(checkbox => {
     this.addEventListener('click', function(){
         let geselecteerdeGoals = document.querySelectorAll('input[type="checkbox"]:checked');
         console.log(geselecteerdeGoals.length)
+
         if(geselecteerdeGoals.length >= 3) {
-            submit.disabled = false
-        }else{
-            console.log('hi')
-            submit.disabled = true
+            selecteerDrie.classList.remove("fat_wiggle")
+            console.log('jup')
         }
+
+        submit.addEventListener('click', function(){
+            let geselecteerdeGoals = document.querySelectorAll('input[type="checkbox"]:checked');
+            console.log(geselecteerdeGoals.length)
+            if(geselecteerdeGoals.length < 3) {
+                checkboxOne.setCustomValidity("Selecteer minimaal 3 items")
+                checkboxOne.reportValidity()
+                console.log('lager dan 3')
+                selecteerDrie.classList.add("fat_wiggle")
+            }else{
+                checkboxOne.setCustomValidity("");
+                console.log('hoger dan 3')
+                // selecteerDrie.classList.remove('fat_wiggle')
+            }
+        })
     })
 })
 
+// submit.forEach(checkbox => {
+//     this.addEventListener('click', function(){
+//         let geselecteerdeGoals = document.querySelectorAll('input[type="checkbox"]:checked');
+//         console.log(geselecteerdeGoals.length)
+//         if(geselecteerdeGoals.length >= 3) {
+//             checkboxOne.setCustomValidity("This field cannot be left blank")
+//         }else{
+//             console.log('hi')
+//             submit.disabled = true
+//         }
+//     })
+// })
 
 let upload = document.querySelector('input[type="file"]')
 let profile = document.querySelector('.profile-picture')
--- server.js
+++ server.js
@@ -46,11 +46,12 @@ app.get('/dashboard/:id', function (request, response) {
 app.get('/klant-toevoegen-gegevens/:id', function (request, response) {  
     Promise.all([
         fetchJson(apiUrl + '/hf_companies/' + request.params.id + '/?fields=*,logo.id,logo.height,logo.width'),
-        fetchJson(apiUrl + `/hf_stakeholders/?filter={"company_id":"${request.params.id}"}`)
-    ]).then(([companiesData, stakeholdersData]) => {
+        fetchJson(apiUrl + `/hf_stakeholders/?filter={"company_id":"${request.params.id}","id":"39"}`)
+    ]).then(([companiesData, klantenData]) => {
         response.render('vragenlijst-gegevens', {
             sdgs: sdgData.data,
-            companies: companiesData.data
+            companies: companiesData.data,
+            klantA: klantenData.data
         })
     })
 })
@@ -58,36 +59,81 @@ app.get('/klant-toevoegen-gegevens/:id', function (request, response) {
 app.get('/klant-toevoegen-sdgs/:id', function (request, response) {  
     Promise.all([
         fetchJson(apiUrl + '/hf_companies/' + request.params.id + '/?fields=*,logo.id,logo.height,logo.width'),
-        fetchJson(apiUrl + `/hf_stakeholders/?filter={"company_id":"${request.params.id}"}`)
-    ]).then(([companiesData, stakeholdersData]) => {
+        fetchJson(apiUrl + `/hf_stakeholders/?filter={"company_id":"${request.params.id}","id":"39"}`)
+    ]).then(([companiesData, klantenData]) => {
         response.render('vragenlijst-sdgs', {
             sdgs: sdgData.data,
-            companies: companiesData.data
+            companies: companiesData.data,
+            klantA: klantenData.data
         })
     })
 })
 
 app.post('/klant-toevoegen-scores/:id', (request, response) => { //post route naar / met response request
     console.log(request.body); // log request body in console
-    const sdgId = request.body.sdg; // haal sdg uit request body
-    console.log(sdgId);
+    const sdgId = request.body.sdg;
+    const selectSdgId = request.body.selectSdg; // haal sdg uit request body
+    // console.log(sdgId);
+
+
+    
+
+
+    
     if (sdgId) {
+        request.body.sdg.forEach(async function(sdg) {
+            await fetch('https://fdnd-agency.directus.app/items/hf_scores', {
+                method: 'POST',
+                body: JSON.stringify({
+                sdg_id: sdg,
+                stakeholder_id: request.body.stakeholder_id, // <-- Hier wil je iets dynamisch, eigenlijk
+                score: 0
+                }),
+                headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+                }
+            })
+        })
         response.redirect(`/klant-toevoegen-scores/${request.params.id}?sdgIds=${sdgId}`); // redirect naar scoreboard net de sdgId
+    }
+    if (selectSdgId) {
+        request.body.selectSdg.forEach(async function(selectSdg) {
+            await fetch('https://fdnd-agency.directus.app/items/hf_scores', {
+                method: 'POST',
+                body: JSON.stringify({
+                    sdg_id: request.body.sdg_id,
+                    stakeholder_id: request.body.stakeholder_id, // <-- Hier wil je iets dynamisch, eigenlijk
+                    score_id: selectSdg
+                }),
+                headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+                }
+            })
+        })
+        console.log(request.body.sdg_id)
+        console.log(request.body.stakeholder_id)
+        console.log(request.body.selectSdg)
+        response.redirect(`/klant-toevoegen-scores/${request.params.id}?sdgIds=${request.body.sdg_id}`); // redirect naar scoreboard net de sdgId
     } else {
         response.redirect('/klant-toevoegen-sdgs/:id?error=true'); // brengt error
     }
+
+
 })
 
 app.get('/klant-toevoegen-scores/:id', function (request, response) {  
-    const filteredSdgs = sdgData.data.filter(sdg => request.query.sdgIds.includes(sdg.number)) // filtert de sdgs die zijn aangekruist bij de checkboxes 
-    console.log(sdgData.data)
+    const sdgIds = request.query.sdgIds.split(','); // split sdgIds als een array
+    const filteredSdgs = sdgData.data.filter(sdg => sdgIds.includes(sdg.number.toString())); // convert sdg.number to string before checking if it's in sdgIds
+    // console.log(sdgData.data)
     Promise.all([
         fetchJson(apiUrl + '/hf_companies/' + request.params.id + '/?fields=*,logo.id,logo.height,logo.width'),
-        fetchJson(apiUrl + `/hf_stakeholders/?filter={"company_id":"${request.params.id}"}`)
-    ]).then(([companiesData, stakeholdersData]) => {
+        fetchJson(apiUrl + `/hf_stakeholders/?filter={"company_id":"${request.params.id}","id":"39"}`)
+    ]).then(([companiesData, klantenData]) => {
+        // console.log(klantenData)
         response.render('vragenlijst-scores', {
             sdgs: filteredSdgs,
-            companies: companiesData.data
+            companies: companiesData.data,
+            klantA: klantenData.data
         })
     })
 })
